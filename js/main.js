817396b1e3900efbffc4e3aa552d73afb0270f72
LeonelCabrera19 <leonelcabrera1992@gmail.com>
2016-05-11 12:11:49 -0600

    Se agrego la funcion para agregar task con solo presionar enter en el input Se agrego la funcion de eliminar task con el nuevo boton Se creo la funcion para comenzar la task       -Boton verde       -Boton rojo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//VARIABLES GLOBALES
+var start = false;
+
 jQuery(document).ready(function(){
     $("#btnAdd").hide();
     loadTasks();
@@ -14,8 +17,11 @@ function showAddButton() {
 }
 
 function addTask() {
-    createTask(localStorage.length,$("#newTask").val());
     localStorage.setItem(localStorage.length,$("#newTask").val());
+    $("#tasks-table tr").remove();
+    loadTasks();
+    $("#newTask").val('');
+    $("#newTask").focus();
 }
 
 function loadTasks(){
@@ -24,6 +30,15 @@ function loadTasks(){
         value = localStorage[key];
         createTask(key,localStorage[key]);
      }
+     
+     $(".btnDelete").click(function(){
+           var $row = $(this).closest("tr"),        // Finds the closest row <tr>
+           $tds = $row.find("td:nth-child(6)")
+           $key = $($tds).text();
+           removeTask($key);
+    });
+    
+    startTask();
 }
 
 function createTask(key,task){
@@ -42,16 +57,16 @@ function createTask(key,task){
     "<span class='task-time'>00:00:00</span>"+
     "</td>"+
     "<td width='15%' align='center'>"+
-    "<button type='button' class='task-time-button green-flat-button'>Start</button>"+
+    "<button type='button' class='task-time-button green-flat-button btnStart'>Start</button>"+
     "</td>"+
     "<td width='10%' align='center'>"+
-    "<i class='fa fa-close inactive' title='Delete'></i>"+
+    "<i class='fa fa-close inactive btnDelete' title='Delete'></i>"+
     "</td>"+
+    "<td hidden='hidden'>"+key+"</td>"+
     "</tr>";
 
     $("#tasks-table").append(taskStructure);
-    $("#newTask").val('');
-    $("#newTask").focus();
+    
 }
 
 function clearTasks(){
@@ -60,4 +75,25 @@ function clearTasks(){
 
 function removeTask(key){
     localStorage.removeItem(key);
+    $("#tasks-table tr").remove();
+    loadTasks();
 }
+
+function startTask(){
+    $(".btnStart").click(function(){
+        if(start==false){
+            $(this).removeClass("green-flat-button");
+            $(this).addClass("red-flat-button");
+            $(this).text("Stop");
+            start = true;
+        }else{
+            $(this).addClass("green-flat-button");
+            $(this).removeClass("red-flat-button");
+            $(this).text("Start");
+            start = false;
+        }
+     });
+}
+
+
+
